0b4ebebb3a9805911c7d6dd769d4556b9c26b22b
ruritoBlogger <ruriro0125@gmail.com>
2022-10-02 22:52:41 +0900

    :sparkles: 仮で建てたgraphqlのエンドポイントを叩くようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,31 @@
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
+import fetchGraphQL from '../lib/fetchGraphQL';
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(
+      `
+      query todos {
+        todo {
+          id
+          title
+          content
+        }
+      }
+    `,
+      {},
+    ).then((response) => {
+      // Avoid updating state if the component unmounted before the fetch completes
+      if (!isMounted) {
+        return;
+      }
+      const data = response.data;
+
+      console.log(data);
+    });
+  });
   return (
     <>
       <p>home</p>
