e2c5860e099626187518c9982823387f3e3aaf12
ruritoBlogger <ruriro0125@gmail.com>
2022-10-02 22:49:23 +0900

    :sparkles: relayを叩く用？のtsファイルを追加

---
+++ src/lib/fetchGraphQL.ts
@@ -0,0 +1,18 @@
+// TODO: 型を厳密にする
+async function fetchGraphQL(text: string, variables: Record<string, any>) {
+  const response = await fetch('/api/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  });
+
+  // Get the response as JSON
+  return await response.json();
+}
+
+export default fetchGraphQL;
