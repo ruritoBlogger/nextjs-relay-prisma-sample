3cdda7838fd74149b92ae042faa2686f94de59d0
ruritoBlogger <ruriro0125@gmail.com>
2022-10-16 16:32:46 +0900

    :recycle: 不要なimportを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
 import type { NextPage } from 'next';
-import { Suspense, useEffect } from 'react';
-import fetchGraphQL from '../lib/fetchGraphQL';
-import {
-  fetchQuery,
-  graphql,
-  loadQuery,
-  PreloadedQuery,
-  usePreloadedQuery,
-} from 'react-relay';
+import { Suspense } from 'react';
+import { loadQuery } from 'react-relay';
 import RelayEnvironment from '../lib/RelayEnvironment';
-import { OperationType } from 'relay-runtime';
 import { TodoList, TodosQuery } from '../components/TodoList';
 
 const Home: NextPage = () => {
