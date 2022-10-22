5d2ed2ad34fee1e13291791dd2e9e62d01470f70
ruritoBlogger <ruriro0125@gmail.com>
2022-10-22 15:25:40 +0900

    :wrench: msw + storybookの設定をした

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,15 @@
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+initialize();
+
+export const decorators = [mswDecorator];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
