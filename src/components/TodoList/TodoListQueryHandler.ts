8d93c940b6cc0adf5751cdab2b0f0dfe31a53853
ruritoBlogger <ruriro0125@gmail.com>
2022-10-22 15:34:21 +0900

    :sparkles: TodoListQueryのmockを書いた

--- src/components/TodoList/TodoListQueryHandler.ts
+++ src/components/TodoList/TodoListQueryHandler.ts
@@ -1,25 +1,43 @@
 import { graphql } from 'msw';
 import { GraphQLError } from 'graphql/error';
+import {
+  TodoListQuery,
+  TodoListQuery$data,
+  TodoListQuery$variables,
+} from '../../graphql/__generated__/TodoListQuery.graphql';
 
 interface TodoListQueryHandlerInput {
+  todos?: TodoListQuery$data;
   errors?: string;
 }
 
-export const todoListQueryHandler = ({ errors }: TodoListQueryHandlerInput) => {
-  return graphql.query('TodoListQuery', (req, res, ctx) => {
-    // TODO: errorの型情報が確定したら、それに合わせて返却する値も調整する
-    if (errors) {
-      return res(ctx.errors([new GraphQLError('error!!!')]));
-    }
+export const todoListQueryHandler = ({
+  todos,
+  errors,
+}: TodoListQueryHandlerInput) => {
+  return graphql.query<TodoListQuery$data, TodoListQuery$variables>(
+    'TodoListQuery',
+    (req, res, ctx) => {
+      // TODO: errorの型情報が確定したら、それに合わせて返却する値も調整する
+      if (errors) {
+        return res(ctx.errors([new GraphQLError(errors)]));
+      }
 
-    return res(
-      ctx.data({
-        todos: {
-          id: 1,
-          title: 'test title',
-          content: 'test content',
-        },
-      }),
-    );
-  });
+      if (todos) {
+        return res(ctx.data(todos));
+      }
+
+      return res(
+        ctx.data({
+          todos: [
+            {
+              id: '1',
+              title: 'test title',
+              content: 'test content',
+            },
+          ],
+        }),
+      );
+    },
+  );
 };
